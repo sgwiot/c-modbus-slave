27b4008f4ed5968391b642b64c3e2d6bbb6768cd
TonyHo <373466062@qq.com>
2021-02-05 11:43:51 +0800

    feat: add a tcp poll server example code

--- src/build.cmd
+++ src/build.cmd
@@ -1,2 +1,3 @@
 gcc server.c -o server -Wall -std=c99 `pkg-config --libs --cflags libmodbus`
 gcc modbus_rs232_slave_server.c -o 32_device -Wall -std=c99 `pkg-config --libs --cflags libmodbus`
+gcc modbus_tcp_master_client.c -o tcp-poll -Wall -std=c99 `pkg-config --libs --cflags libmodbus` 
