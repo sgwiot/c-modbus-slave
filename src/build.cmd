c685d4a63e2b9f6e2823d31165e902ee92d72854
TonyHo <373466062@qq.com>
2021-02-05 18:16:26 +0800

    feat: add ini examples code

--- src/build.cmd
+++ src/build.cmd
@@ -3,3 +3,5 @@ gcc modbus_rs232_slave_server.c -o 32_device -Wall -std=c99 `pkg-config --libs -
 gcc modbus_tcp_master_client.c -o tcp-poll -Wall -std=c99 `pkg-config --libs --cflags libmodbus` 
 gcc tcp-slave.c -o tcp-slave -Wall -std=c99 `pkg-config --libs --cflags libmodbus`
 gcc modbus_tcp_slave_server.c -o tcp-slave-temp -Wall -std=c99 `pkg-config --libs --cflags libmodbus`
+gcc ini_example.c ini.c  -o init_example
+gcc ini.c ini_xmacros.c -o mac
