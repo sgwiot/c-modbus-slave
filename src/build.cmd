e83d6894a0e31f780fd556467232e38a4b259128
TonyHo <373466062@qq.com>
2021-02-05 15:17:18 +0800

    feat: add tcp example, the first one would update register value using cpu temprature

--- src/build.cmd
+++ src/build.cmd
@@ -1,3 +1,5 @@
 gcc server.c -o server -Wall -std=c99 `pkg-config --libs --cflags libmodbus`
 gcc modbus_rs232_slave_server.c -o 32_device -Wall -std=c99 `pkg-config --libs --cflags libmodbus`
 gcc modbus_tcp_master_client.c -o tcp-poll -Wall -std=c99 `pkg-config --libs --cflags libmodbus` 
+gcc tcp-slave.c -o tcp-slave -Wall -std=c99 `pkg-config --libs --cflags libmodbus`
+gcc modbus_tcp_slave_server.c -o tcp-slave-temp -Wall -std=c99 `pkg-config --libs --cflags libmodbus`
